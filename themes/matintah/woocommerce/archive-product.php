3670251dba5e27a754f8a6b844f5c617ed082964
viniciusofp <viniciusofp@gmail.com>
2017-02-12 19:16:08 -0200

    v1.1

---
+++ themes/matintah/woocommerce/archive-product.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * The Template for displaying product archives, including the main shop page which is a post type archive
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/archive-product.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     2.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+get_header( 'shop' ); ?>
+<div id="fullpage">
+	<section>
+
+			<div style="height: 100%; width: 100%; background: url('http://matintah.com/wp-content/uploads/2016/12/matintah_testes-7.jpg') 50% 0 no-repeat fixed; background-size: 100% auto" class="parallax" data-velocity="-2">
+				
+			</div>
+
+
+	</section>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="spacer"></div>
+			<?php
+				/**
+				 * woocommerce_before_main_content hook.
+				 *
+				 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+				 * @hooked woocommerce_breadcrumb - 20
+				 */
+				do_action( 'woocommerce_before_main_content' );
+			?>
+
+				<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+
+					<h1 class="page-title"><?php woocommerce_page_title(); ?></h1>
+
+				<?php endif; ?>
+
+				<?php
+					/**
+					 * woocommerce_archive_description hook.
+					 *
+					 * @hooked woocommerce_taxonomy_archive_description - 10
+					 * @hooked woocommerce_product_archive_description - 10
+					 */
+					do_action( 'woocommerce_archive_description' );
+				?>
+			</div>
+
+
+				<?php if ( have_posts() ) : ?>
+					<div class="col-md-12">
+						<div class="spacer"></div>
+						<?php
+							/**
+							 * woocommerce_before_shop_loop hook.
+							 *
+							 * @hooked woocommerce_result_count - 20
+							 * @hooked woocommerce_catalog_ordering - 30
+							 */
+							do_action( 'woocommerce_before_shop_loop' );
+						?>
+						<div class="spacer"></div>
+					</div>
+
+					<?php woocommerce_product_loop_start(); ?>
+
+						<?php woocommerce_product_subcategories(); ?>
+
+						<?php while ( have_posts() ) : the_post(); ?>
+							
+							<div class="col-sm-4">
+								<?php wc_get_template_part( 'content', 'product' ); ?>
+							</div>
+
+						<?php endwhile; // end of the loop. ?>
+
+					<?php woocommerce_product_loop_end(); ?>
+
+					<?php
+						/**
+						 * woocommerce_after_shop_loop hook.
+						 *
+						 * @hooked woocommerce_pagination - 10
+						 */
+						do_action( 'woocommerce_after_shop_loop' );
+					?>
+
+				<?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
+
+					<?php wc_get_template( 'loop/no-products-found.php' ); ?>
+
+				<?php endif; ?>
+
+			<?php
+				/**
+				 * woocommerce_after_main_content hook.
+				 *
+				 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+				 */
+				do_action( 'woocommerce_after_main_content' );
+			?>
+
+			<?php
+				/**
+				 * woocommerce_sidebar hook.
+				 *
+				 * @hooked woocommerce_get_sidebar - 10
+				 */
+				do_action( 'woocommerce_sidebar' );
+			?>		
+			</div>
+		</div>
+	</div>
+</div>
+	
+	
+
+<?php get_footer( 'shop' ); ?>
